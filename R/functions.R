ca1eac8ee62c166c9b329964f905f3ec539620f4
Julie Vercelloni <julie.vercelloni@gmail.com>
2025-02-14 14:57:00 +1000

    data processing

--- R/functions.R
+++ R/functions.R
@@ -1,15 +1,17 @@
 ## -----  functions 
 slice_duplicates <- function(df) {
   df |>
-    group_by(Longitude, Latitude, image_name) |>         
-    filter(n() > 1) |>       
-    slice(1) |>              
-    ungroup()
+    group_by(Longitude, Latitude, image_name) |> 
+    nest() |>
+    dplyr::select(-data) |>
+    count() |>       
+    filter(n() > 1) |>
+    data.frame()
 }
 
 clean_data <- function(df) {
   df |>
-      group_by_all() |>
+    group_by_all() |>
     mutate(is_duplicate = n() > 1) |> # Mark duplicates
     filter(!is_duplicate | row_number() == 1) |> # Keep only one row from duplicates
     select(-is_duplicate) |> # Drop helper column
